d3b6aba7eeacaedaaeca4246e03dafdd28e0c865
Maria Mazur <maria.mazur@teaminternet.com>
2022-09-29 15:26:43 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function toggleMobileNav() {
+    let topNav = document.getElementById("topNav")
+    let imgReplace = document.getElementById("menuImg");
+    if (topNav.classList.contains('nav-closed')) {
+        topNav.classList.remove('nav-closed')
+        topNav.classList.add('nav-open')
+        imgReplace.src = "./img/style/close_black.svg";
+    } else if (topNav.classList.contains('nav-open')) {
+        topNav.classList.remove('nav-open')
+        topNav.classList.add('nav-closed')
+        imgReplace.src = "./img/style/menu.svg";
+    }
+}
